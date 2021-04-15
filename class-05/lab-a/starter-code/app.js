1f87b7c6a15feabb730c4ab761b8ca27252b5731
salammustafa728 <salammustafa728@gmail.com>
2021-04-15 16:18:27 +0300

    I solved the SumArr function

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -83,12 +83,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+let s = sum(sumArr[0],sumArr[1]);
+//console.log('this is sum of the 1 and 2 in arr',s[0]);
+let s2 = sum(s[0],sumArr[2]);
+//console.log('this is sum of the s and 3 in arr',s2[0]);
+let msg = sumArr[0]+','+sumArr[1]+','+sumArr[2] +' was passed in as an array of numbers, and '+ s2[0] +' is their sum.';
+return[s2[0],msg];
 }
-
+console.log(sumArray(testArray));
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
